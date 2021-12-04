3e9068276decb0456a085c438770d8fc3e487c20
Cylas Kiganda <kigandacylas@gmail.com>
2021-12-04 20:50:43 +0300

    Add files via upload

---
+++ PFS-payment-gateway.php
@@ -0,0 +1,174 @@
+<?php
+/*
+   Plugin Name: Custom Payment Gateway(P.F.S)
+  Description: It allows customers to pay from the store
+  Description: Custom pay on pickup payment.
+  Version: 1.1
+  Author: Cylas Kiganda
+	WC requires at least: 3.5
+  WC tested up to: 3.5
+  Text Domain: wc-gateway-PFS
+*/
+ 
+defined( 'ABSPATH' ) or exit;
+
+
+// Make sure WooCommerce is active
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	return;
+}
+
+
+
+
+/**
+ * PFS Payment Gateway
+ *
+ * Provides an PFS Payment Gateway; mainly for testing purposes.
+ * We load it later to ensure WC is loaded first since we're extending it.
+ */
+add_action( 'plugins_loaded', 'wc_PFS_gateway_init' );
+
+function wc_PFS_gateway_init() {
+
+	class WC_Gateway_PFS extends WC_Payment_Gateway {
+
+		/**
+		 * Constructor for the gateway.
+		 */
+		public function __construct() {
+	  
+			$this->id                 = 'PFS_gateway_pick';
+			$this->icon               = apply_filters('woocommerce_bacs_icon', '');
+			$this->has_fields         = false;
+			$this->method_title       = __( 'Pay From the store', 'wc-gateway-PFS' );
+			$this->method_description = __( 'Allows PFS payments. Very handy!!. Orders are marked as "on-hold" when received.', 'woocommerce' );
+		  
+			// Load the settings.
+			$this->init_form_fields();
+			$this->init_settings();
+		  
+			// Define user set variables
+			$this->title        = $this->get_option( 'title' );
+			$this->description  = $this->get_option( 'description' );
+			$this->instructions = $this->get_option( 'instructions', $this->description );
+		  
+			// Actions
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+			add_action( 'woocommerce_thankyou_' . $this->id, array( $this, 'thankyou_page' ) );
+		  
+			// Customer Emails
+			//	add_action( 'woocommerce_email_before_order_table', array( $this, 'email_instructions' ), 10, 3 );
+		}
+	
+	
+		/**
+		 * Initialize Gateway Settings Form Fields
+		 */
+		public function init_form_fields() {
+	  
+			$this->form_fields = apply_filters( 'wc_PFS_form_fields', array(
+		  
+				'enabled' => array(
+					'title'   => __( 'Enable/Disable', 'woocommerce' ),
+					'type'    => 'checkbox',
+					'label'   => __( 'Enable PFS Payment', 'woocommerce' ),
+					'default' => 'yes'
+				),
+				
+				'title' => array(
+					'title'       => __( 'Title', 'woocommerce' ),
+					'type'        => 'text',
+					'description' => __( 'This controls the title for the payment method the customer sees during checkout.', 'woocommerce' ),
+					'default'     => __( 'Pay at the Store', 'woocommerce' ),
+					'desc_tip'    => true,
+				),
+				
+				'description' => array(
+					'title'       => __( 'Description', 'woocommerce' ),
+					'type'        => 'textarea',
+					'description' => __( 'Payment method description that the customer will see on your checkout.', 'woocommerce' ),
+					'default'     => __( 'Please make payment from the Store.', 'woocommerce' ),
+					'desc_tip'    => true,
+				),
+				
+				'instructions' => array(
+					'title'       => __( 'Instructions', 'woocommerce' ),
+					'type'        => 'textarea',
+					'description' => __( 'Instructions that will be added to the thank you page and emails.', 'woocommerce' ),
+					'default'     => '',
+					'desc_tip'    => true,
+				),
+			) );
+		}
+	
+	
+		/**
+		 * Output for the order received page.
+		 */
+		public function thankyou_page() {
+			if ( $this->instructions ) {
+				echo wpautop( wptexturize( $this->instructions ) );
+			}
+		}
+	
+	
+		/**
+		 * Add content to the WC emails.
+		 *
+		 * @access public
+		 * @param WC_Order $order
+		 * @param bool $sent_to_admin
+		 * @param bool $plain_text
+		 */
+		public function email_instructions( $order, $sent_to_admin, $plain_text = false ) {
+		
+			if ( $this->instructions && ! $sent_to_admin && $this->id === $order->payment_method && $order->has_status( 'on-hold' ) ) {
+				echo wpautop( wptexturize( $this->instructions ) ) . PHP_EOL;
+			}
+		}
+	
+	
+		/**
+		 * Process the payment and return the result
+		 *
+		 * @param int $order_id
+		 * @return array
+		 */
+		public function process_payment( $order_id ) {
+	
+			$order = wc_get_order( $order_id );
+			
+			// Mark as on-hold (we're awaiting the payment)
+			$order->update_status( 'on-hold', __( 'Awaiting PFS payment', 'wc-gateway-PFS' ) );
+			
+			// Reduce stock levels
+			//$order->reduce_order_stock();
+			
+			// Remove cart
+			WC()->cart->empty_cart();
+			
+			// Return thankyou redirect
+			return array(
+				'result' 	=> 'success',
+				'redirect'	=> $this->get_return_url( $order )
+			);
+		}
+		
+	
+  } 
+  
+/**
+ * Add the gateway to WC Available Gateways
+ * 
+ * @since 1.0.0
+ * @param array $gateways all available WC gateways
+ * @return array $gateways all WC gateways + PFS gateway
+ */
+function wc_PFS_add_to_gateways( $gateways ) {
+	$gateways[] = 'WC_Gateway_PFS';
+	return $gateways;
+}
+add_filter( 'woocommerce_payment_gateways', 'wc_PFS_add_to_gateways' );
+
+}
